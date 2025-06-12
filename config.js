655138528b430d03c5c3d584c0ced6fb8d46bb48
Egodev <xhe5@duck.com>
2025-06-12 21:04:06 +0200

    config.js

--- config.js
+++ config.js
@@ -1,4 +1,5 @@
 // Welcome to the configuration file for Pandy.
+// https://github.com/EgooDev/PandyAI
 // To get our API keu, please visit https://openrouter.ai/settings/keys and create an account.
 // Replace the API_KEY below with your own key.
 // Example: const API_KEY = "API-FAKEAPIEXAMPLEFORPANDY";
