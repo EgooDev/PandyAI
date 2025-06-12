caf3568478c531c4d3f65228b06aac27359a0a18
Egodev <xhe5@duck.com>
2025-06-12 20:47:36 +0200

    Add files via upload

---
+++ config.js
@@ -0,0 +1,4 @@
+// Welcome to the configuration file for Pandy.
+// To get our API keu, please visit https://openrouter.ai/settings/keys and create an account.
+// Replace the API_KEY below with your own key.
+const API_KEY = "";
