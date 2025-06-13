5cdb74f6effffe4e68d56d38d280cb52c66f3e57
Egodev <xhe5@duck.com>
2025-06-13 20:10:39 +0200

    Add files via upload

---
+++ system/system.js
@@ -0,0 +1,14 @@
+// Don"t modify this file directly if you don't know what you're doing.
+// Don"t modify this file directly if you don't know what you're doing.
+// Don"t modify this file directly if you don't know what you're doing.
+// Don"t modify this file directly if you don't know what you're doing.
+// Don"t modify this file directly if you don't know what you're doing.
+// Don"t modify this file directly if you don't know what you're doing.
+// Don"t modify this file directly if you don't know what you're doing.
+// Don"t modify this file directly if you don't know what you're doing.
+// Don"t modify this file directly if you don't know what you're doing.
+// Don"t modify this file directly if you don't know what you're doing.
+// Don"t modify this file directly if you don't know what you're doing.
+const MAX_CHATS = 10; 
+const API_URL = "https://openrouter.ai/api/v1/chat/completions";
+const MODEL = "deepseek/deepseek-r1-0528:free";
